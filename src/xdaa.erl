f07dd51c7955fcee6a8837717cad334187a46bb4
Zane Beckwith <zanebeckwith@aol.com>
2017-08-01 16:17:07 +0000

    Swap-bytes for ECDHE public keys.

--- src/xdaa.erl
+++ src/xdaa.erl
@@ -72,12 +72,12 @@ xdaa_wait_on_server_key_exchange(TCPSocket, ServerGIDLength, ServerNonceLength, 
         case gen_tcp:recv(TCPSocket, ServerGIDLength+ServerNonceLength+ServerECDHEPubKeyLength+ServerSigLength, ?TIMEOUT) of
                 {ok, <<ServerGID:ServerGIDLength/binary-unit:8,
                        ServerNonce:ServerNonceLength/binary-unit:8,
-                       ServerECDHEPubKey:ServerECDHEPubKeyLength/binary-unit:8,
+                       ServerECDHEPubKeySwapped:ServerECDHEPubKeyLength/binary-unit:8,
                        ServerSig:ServerSigLength/binary-unit:8>>
                 } ->
-                        lager:debug("Received XDAA ServerKeyExchange with GID:~p, Nonce:~p, PubKey:~p, and Sig:~p",
-                                    [ServerGID, ServerNonce, ServerECDHEPubKey, ServerSig]),
-                        xdaa_validate_server_gid(TCPSocket, ServerGID, ServerNonce, ServerECDHEPubKey, ServerSig, ClientNonce);
+                        lager:debug("Received XDAA ServerKeyExchange with GID:~p, Nonce:~p, SwappedPubKey:~p, and Sig:~p",
+                                    [ServerGID, ServerNonce, ServerECDHEPubKeySwapped, ServerSig]),
+                        xdaa_validate_server_gid(TCPSocket, ServerGID, ServerNonce, ServerECDHEPubKeySwapped, ServerSig, ClientNonce);
                 {ok, _} ->
                         lager:warning("XDAA: Received mal-formed ServerKeyExchange", []),
                         {error, "XDAA: Mal-formed ServerKeyExchange message"};
@@ -85,20 +85,20 @@ xdaa_wait_on_server_key_exchange(TCPSocket, ServerGIDLength, ServerNonceLength, 
                         {error, Reason}
         end.
 
-xdaa_validate_server_gid(TCPSocket, ServerGID, ServerNonce, ServerECDHEPubKey, ServerSig, ClientNonce) ->
+xdaa_validate_server_gid(TCPSocket, ServerGID, ServerNonce, ServerECDHEPubKeySwapped, ServerSig, ClientNonce) ->
         %% TODO: Verify GID is in known list
         GIDVerify = ok,
         case GIDVerify of
                 ok ->
                         lager:debug("Server GID (~p) accepted", [ServerGID]),
-                        xdaa_validate_server_signature(TCPSocket, ServerGID, ServerNonce, ServerECDHEPubKey, ServerSig, ClientNonce);
+                        xdaa_validate_server_signature(TCPSocket, ServerGID, ServerNonce, ServerECDHEPubKeySwapped, ServerSig, ClientNonce);
                 {error, Reason} ->
                         lager:info("XDAA: Server GID (~p) rejected", [ServerGID]),
                         {error, Reason}
         end.
 
-xdaa_validate_server_signature(TCPSocket, ServerGID, ServerNonce, ServerECDHEPubKey, ServerSig, ClientNonce) ->
-        SigStruct = <<ServerECDHEPubKey/binary, ClientNonce/binary>>,
+xdaa_validate_server_signature(TCPSocket, ServerGID, ServerNonce, ServerECDHEPubKeySwapped, ServerSig, ClientNonce) ->
+        SigStruct = <<ServerECDHEPubKeySwapped/binary, ClientNonce/binary>>,
         %% TODO: Read this from file.
         ServerECDSAPubKey = 16#04D676C0F20309A1060A15ADBF20A28C27494908F13F34E3DA1F4973DD19B62DA281BBDEEB0D00354CD0A0E437958DE2BBD32BA70FE8FF805D9C395A1E069CDCEB,
         case crypto:verify(ecdsa,
@@ -108,41 +108,49 @@ xdaa_validate_server_signature(TCPSocket, ServerGID, ServerNonce, ServerECDHEPub
                            [ServerECDSAPubKey, secp256r1] ) of
                 true ->
                         lager:debug("XDAA: Validation of server signature successful", []),
-                        xdaa_send_client_key_exchange(TCPSocket, ServerNonce, ServerECDHEPubKey);
+                        xdaa_send_client_key_exchange(TCPSocket, ServerNonce, ServerECDHEPubKeySwapped);
                 false ->
                         lager:warning("XDAA: Validation of server signature failed", []),
                         {error, "XDAA: Validation of server signature failed"}
         end.
 
-xdaa_send_client_key_exchange(TCPSocket, ServerNonce, ServerECDHEPubKey) ->
+xdaa_send_client_key_exchange(TCPSocket, ServerNonce, ServerECDHEPubKeySwapped) ->
         % Generate ECDHE key pair
         #{public := ClientECDHEPubKey, secret := ClientECDHEPrivKey} = enacl:kx_keypair(),
+        ClientECDHEPubKeySwapped = reverse_bytes(ClientECDHEPubKey),
 
         % Generate signature (and its length) from crypto
-        SigStruct = <<ClientECDHEPubKey/binary, ServerNonce/binary>>,
+        SigStruct = <<ClientECDHEPubKeySwapped/binary, ServerNonce/binary>>,
         ClientECDSAPrivKey = 16#43F8F422782842DE799239644A49E359FDBFF81AB604AFD8BE0089B4F3686A0C,
         Signature = crypto:sign(ecdsa,
                                 sha256,
                                 SigStruct,
                                 [ClientECDSAPrivKey, secp256r1]),
-        ECDHEPubKeyLength = bit_size(ClientECDHEPubKey) div 8,
+        ECDHEPubKeyLength = bit_size(ClientECDHEPubKeySwapped) div 8,
         SigLength = bit_size(Signature) div 8,
 
         Packet = <<?XDAA_VERSION:8,
                    ECDHEPubKeyLength:16/big,
                    SigLength:16/big,
-                   ClientECDHEPubKey/binary,
+                   ClientECDHEPubKeySwapped/binary,
                    Signature/binary>>,
         lager:debug("Sending XDAA ClientKeyExchange (~p)...", [Packet]),
         case gen_tcp:send(TCPSocket, Packet) of
                 ok ->
                         lager:debug("Sent XDAA ClientKeyExchange (~p) of length ~p octets", [Packet, bit_size(Packet) div 8]),
-                        xdaa_run_diffie_hellman(TCPSocket, ServerECDHEPubKey, ClientECDHEPrivKey);
+                        xdaa_run_diffie_hellman(TCPSocket, ServerECDHEPubKeySwapped, ClientECDHEPrivKey);
                 {error, Reason} ->
                         {error, Reason}
         end.
 
-xdaa_run_diffie_hellman(TCPSocket, ServerECDHEPubKey, ClientECDHEPrivKey) ->
+xdaa_run_diffie_hellman(TCPSocket, ServerECDHEPubKeySwapped, ClientECDHEPrivKey) ->
+        ServerECDHEPubKey = reverse_bytes(ServerECDHEPubKeySwapped),
         DHSharedSecret = enacl:curve25519_scalarmult(ClientECDHEPrivKey, ServerECDHEPubKey),
         lager:debug("Got shared-secret:~p", [DHSharedSecret]),
         {ok, TCPSocket}.
+
+reverse_bytes(Input) ->
+        %% Interpret input as little-endian, then re-interpret as big-endian.
+        Size = size(Input),
+        <<AsLittle:Size/little-unit:8>> = Input,
+        <<AsLittle:Size/big-unit:8>>.
